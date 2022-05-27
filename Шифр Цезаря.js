c6192d249835d2405487f9adc317cbbdeea8c4b1
Andrei <andrrei17@gmail.com>
2022-05-27 14:54:57 +0300

    Final cut

--- Шифр Цезаря.js
+++ Шифр Цезаря.js
@@ -1,11 +1,8 @@
 let num = [1, 2, 3, 8, 4];
-let letters = ['a', 'b', 'c'];
 
 function Cezar (arr) {
-for (let i = 0; i < arr.length; i++) {
-    arr[i] += next.letter();
-}
-return arr;
+let number = arr.map(num => num + 2);
+return number;
 }
 
-console.log(Cezar(letters));
+console.log(Cezar(num));
